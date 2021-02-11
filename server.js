37d9fe47c018f585be63939f25d09f73b23ff286
samuelkivi <samuel.kivi98@gmail.com>
2021-02-11 12:35:03 +0200

    Added connection to mysql and added connection pooling

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var mysql = require('mysql');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -6,6 +5,16 @@ var cors = require('cors')
 const express = require('express')
 const app = express()
 const port = 3001
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: 'rootpassword',
+	database: 'user_database'
+});
+
 
 app.use(cors());
 
@@ -21,19 +30,24 @@ app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
-app.post('/auth', function (request, response) {
+
+app.post('/save', function (request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	console.log(username, password);
 	if (username && password) {
-		response.send('ookoo',username,password);
+		var sql = "INSERT INTO users (username, pword) VALUES ('" + username + "', '" + password + "')";
+		pool.query(sql, function (err, result, fields) {
+			if (err) throw err;
+			console.log("Number of records inserted: " + result.affectedRows);
+		});
+		response.send('ookoo');
 		response.end();
 	} else {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
 });
-
 
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`)
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import {
-  BrowserRouter as Router,
-  Switch, Route, Link
-} from "react-router-dom";
 import axios from 'axios';
-import { createHashHistory } from 'history'
 import "./Login.css";
 
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  //Browser history
-  const history = createHashHistory()
 
   //Checks that there is username and password
   function validateForm() {
@@ -26,40 +18,39 @@ export default function Login() {
     event.preventDefault();
   }
 
-  function handleClick(e) {
+  function handleSave(e) {
+    console.log("handleSave")
     e.preventDefault();
     console.log(username, password, "toimii");
 
     //Sends post to server.js
-    axios.post('http://localhost:3001/auth', {
+    axios.post('http://localhost:3001/save', {
       username: username,
       password: password
     })
       //Response from the server
       .then((response) => {
         console.log(response);
-        if(response == "ookoo",username,password){
-          console.log("You in");
-          history.push("/Home");
+        if(response.data == "ookoo"){
+          console.log("Username and password saved");
         }
         else{
-          console.log("Wrong username or password");
+          console.log("Username and password not saved");
         }
       }, (error) => {
         console.log(error);
       });
+  }
 
-      
-
-    //if true router
-    //else nothing
+  function handleShow(e) {
+    console.log("handleShow")
   }
 
   return (
     <div className="Login">
       <Form onSubmit={handleSubmit}>
         <Form.Group size="lg" controlId="username">
-          <Form.Label>username</Form.Label>
+          <Form.Label>Username</Form.Label>
           <Form.Control
             autoFocus
             type="username"
@@ -75,8 +66,11 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()} onClick={handleClick}>
-          Login
+        <Button block size="lg" type="submit" disabled={!validateForm()} onClick={handleSave}>
+          Save to the database
+        </Button>
+        <Button block size="lg" type="submit" onClick={handleShow}>
+          Show database
         </Button>
       </Form>
     </div>
